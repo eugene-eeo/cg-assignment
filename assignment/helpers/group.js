0560f28b02d1652f1bee59ea569bd45d3df21cdf
Eeo Jun <141bytes@gmail.com>
2019-03-11 19:31:53 +0000

    speed up

--- assignment/helpers/group.js
+++ assignment/helpers/group.js
@@ -3,6 +3,7 @@ function drawableTree(d) {
     this.children = [];
     this.f = () => {}; // apply only to our drawable
     this.g = () => {}; // apply to us and our children
+    this.cached = false;
 }
 
 drawableTree.prototype = {
@@ -13,15 +14,28 @@ drawableTree.prototype = {
         this.children.push(dt);
         return dt;
     },
+    invalidate_cache: function() {
+        // initially recursive but we need to keep it fast
+        var s = [this];
+        while (s.length > 0) {
+            var d = s.pop();
+            d.cached = false;
+            for (var i = 0; i < d.children.length; i++)
+                s.push(d.children[i]);
+        }
+    },
     transform: function(f) {
         this.f = f;
-        this.cached = false;
+        this.invalidate_cache();
     },
     grouped: function(g) {
         this.g = g;
-        this.cached = false;
+        this.invalidate_cache();
     },
     apply_transforms: function(fns) {
+        // if we're cached then don't even bother
+        if (this.cached) return;
+        this.cached = true;
         // put our transformation on the stack
         fns.push(this.g);
         // transform our own drawable first
@@ -37,13 +51,15 @@ drawableTree.prototype = {
         // remove our transformation
         fns.pop();
     },
-    draw: function(gl, at) {
-        // first transform
-        if (!at)
-            this.apply_transforms([]);
+    just_draw: function(gl) {
+        // just draw to gl please
         if (this.drawable)
             this.drawable.draw(gl);
         for (var i = 0; i < this.children.length; i++)
-            this.children[i].draw(gl, true);
+            this.children[i].just_draw(gl);
+    },
+    draw: function(gl) {
+        this.apply_transforms([]);
+        this.just_draw(gl);
     },
 };
